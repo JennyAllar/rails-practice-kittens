ed95c2de916a7cdfd15df7ed2a151ce1255620b4
Jenny Allar <JennyAllar@gmail.com>
2014-07-28 22:08:37 -0600

    User can add categorizations to kittens.

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -3,5 +3,9 @@ class KittensController < ApplicationController
   def index
     @kittens = Kitten.all
   end
+  
+  def show
+    
+  end
 
 end
--- spec/features/kitten_spec.rb
+++ spec/features/kitten_spec.rb
@@ -3,7 +3,6 @@ require 'rails_helper'
 feature 'Kittens' do
 
   scenario 'User can categorize kittens' do
-    pending
     user = create_user email: "user@example.com"
     Category.create!(name: "Cutest!")
     Kitten.create!(image: "http://i.imgur.com/tOzb0dUb.jpg")
@@ -14,7 +13,7 @@ feature 'Kittens' do
     find(".kitten-link").click
     select "Cutest!", from: "categorization_category_id"
     click_on "Add Category"
-
+    
     expect(page.current_path).to eq(root_path)
     within(".kitten") do
       expect(page).to have_content("Cutest!")
